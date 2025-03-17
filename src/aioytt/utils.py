44bf3f74ca4e81b75f6900fdde683df7d94cb1ff
narumi <toucans-cutouts0f@icloud.com>
2025-03-18 03:55:34 +0800

    🔥 chore(utils): remove unused utility functions

--- src/aioytt/utils.py
+++
@@ -1,8 +0,0 @@
-import json
-from pathlib import Path
-from typing import Any
-
-
-def save_json(obj: Any, f: str | Path) -> None:
-    with Path(f).open("w") as fp:
-        json.dump(obj, fp, indent=4, ensure_ascii=False)
