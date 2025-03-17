5f4d5e7d3da14a3cb6dd5e585f72ae24cac30e8a
narumi <toucans-cutouts0f@icloud.com>
2025-03-18 03:54:42 +0800

    ✨ feat(video_id): add video ID parsing functionality

--- src/aioytt/__init__.py
+++ src/aioytt/__init__.py
@@ -6,6 +6,7 @@ from loguru import logger
 
 from .transcript import get_transcript_from_url
 from .transcript import get_transcript_from_video_id
+from .video_id import parse_video_id
 
 LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO")
 logger.configure(handlers=[{"sink": sys.stderr, "level": LOGURU_LEVEL}])
